146e25b4b5e972f80255f74a1cb1ac70f80a9239
alfabjostron <23106249+alfabjostron@users.noreply.github.com>
2016-11-24 16:18:00 +0700

    test: cover rename tracking

---
+++ viewer/test/node-shims.d.ts
@@ -0,0 +1,24 @@
+/**
+ * Minimal ambient declarations for the tiny slice of Node.js the tests use.
+ *
+ * repohelix's viewer intentionally ships with zero runtime and (near) zero dev
+ * dependencies. Rather than pulling the full `@types/node` package just to run
+ * a handful of assertions, we declare exactly what the test file touches.
+ */
+
+declare module "node:assert/strict" {
+  interface StrictAssert {
+    (value: unknown, message?: string): void;
+    equal(actual: unknown, expected: unknown, message?: string): void;
+    notEqual(actual: unknown, expected: unknown, message?: string): void;
+    deepEqual(actual: unknown, expected: unknown, message?: string): void;
+    match(value: string, regexp: RegExp, message?: string): void;
+    ok(value: unknown, message?: string): void;
+  }
+  const assert: StrictAssert;
+  export default assert;
+}
+
+declare const console: {
+  log(...args: unknown[]): void;
+  error(...args: unknown[]): void;
