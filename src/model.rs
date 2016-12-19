0130284ec063dcc564b4e35643c0f98404f979d9
alfabjostron <23106249+alfabjostron@users.noreply.github.com>
2016-12-19 17:03:00 +0700

    build: file identity

--- src/model.rs
+++ src/model.rs
@@ -7,3 +7,10 @@
 /// A single file's change within one commit.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct FileChange {
+    /// Repository-relative path of the file after the change.
+    pub path: String,
+    /// Lines added (from `git log --numstat`). `None` for binary files.
+    pub added: Option<u64>,
+    /// Lines removed. `None` for binary files.
+    pub removed: Option<u64>,
+}
