20e13d51cc018ef3870aee5fbf984d4d94f41c16
alfabjostron <23106249+alfabjostron@users.noreply.github.com>
2016-12-27 13:45:00 +0700

    build: rename tracking

--- src/parse.rs
+++ src/parse.rs
@@ -14,3 +14,15 @@ use crate::model::{Commit, FileChange, History};
 /// Parse a raw log string into a [`History`].
 ///
 /// Malformed records are skipped defensively rather than aborting the whole
+/// parse, so a single corrupt line cannot break an otherwise valid import.
+pub fn parse_log(raw: &str) -> History {
+    let mut commits = Vec::new();
+
+    // Records are separated by COMMIT_SEP. The first split segment before the
+    // first separator is empty (or leading whitespace) and is ignored.
+    for record in raw.split(COMMIT_SEP) {
+        let record = record.trim_matches(['\n', '\r']);
+        if record.is_empty() {
+            continue;
+        }
+        if let Some(commit) = parse_record(record) {
