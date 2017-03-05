a7df5a98e77036f383cf542d5070368c5d5e64bd
alfabjostron <23106249+alfabjostron@users.noreply.github.com>
2017-03-05 09:39:00 +0700

    feat: helix bands

---
+++ src/json.rs
@@ -0,0 +1,20 @@
+//! Minimal JSON value builder and serializer.
+//!
+//! `repohelix` avoids external crates, so this module provides just enough
+//! JSON support to emit machine-readable reports. It handles objects, arrays,
+//! strings, numbers, booleans and null with correct escaping and stable,
+//! insertion-ordered object keys (important for deterministic output).
+
+use std::fmt::Write as _;
+
+/// A JSON value. Object key order is preserved as inserted so that report
+/// output is byte-for-byte stable across runs on the same input.
+#[derive(Debug, Clone, PartialEq)]
+pub enum Json {
+    Null,
+    Bool(bool),
+    /// Integers are stored separately to avoid float formatting artifacts.
+    Int(i64),
+    Float(f64),
+    Str(String),
+    Array(Vec<Json>),
