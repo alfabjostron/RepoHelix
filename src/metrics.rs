e2e45f5664cd13582e55d2855f61628d49ca1705
alfabjostron <23106249+alfabjostron@users.noreply.github.com>
2017-07-16 09:27:00 +0700

    chore: rename tracking

---
+++ src/metrics.rs
@@ -0,0 +1,61 @@
+//! Analytical metrics derived from a [`History`].
+//!
+//! Every metric here is a descriptive summary of *file* and *change* activity.
+//! None of these numbers is a judgment about people. Ownership concentration,
+//! for example, measures how change activity for a file is distributed across
+//! author identities — it describes knowledge-distribution risk (bus factor),
+//! not individual performance. See `docs/MODEL.md` for the full rationale.
+
+use crate::model::{Commit, History};
+use std::collections::{BTreeMap, BTreeSet, HashMap};
+
+/// Per-file churn and touch statistics.
+#[derive(Debug, Clone, PartialEq)]
+pub struct FileStat {
+    pub path: String,
+    /// Number of commits that touched this file.
+    pub commits: u64,
+    /// Total lines added across history.
+    pub added: u64,
+    /// Total lines removed across history.
+    pub removed: u64,
+    /// Total churn (added + removed, binaries count as 1 each touch).
+    pub churn: u64,
+    /// Distinct author identities that touched this file.
+    pub authors: u64,
+    /// First (oldest) timestamp the file was touched.
+    pub first_seen: i64,
+    /// Last (newest) timestamp the file was touched.
+    pub last_seen: i64,
+}
+
+/// A co-change relationship between two files.
+#[derive(Debug, Clone, PartialEq)]
+pub struct CoChange {
+    pub a: String,
+    pub b: String,
+    /// Commits in which both files changed together.
+    pub together: u64,
+    /// Commits touching `a`.
+    pub count_a: u64,
+    /// Commits touching `b`.
+    pub count_b: u64,
+    /// Jaccard-like coupling strength in [0, 1]:
+    /// together / (count_a + count_b - together).
+    pub strength: f64,
+    /// Confidence: P(b changes | a changes) = together / count_a, using the
+    /// more frequently changed file as the antecedent for stability.
+    pub confidence: f64,
+}
+
+/// A hotspot combines high churn with high change frequency. Files that are
+/// both large-churn and frequently touched tend to concentrate maintenance
+/// effort and risk.
+#[derive(Debug, Clone, PartialEq)]
+pub struct Hotspot {
+    pub path: String,
+    pub churn: u64,
+    pub commits: u64,
+    /// Normalized score in [0, 1]: geometric blend of normalized churn and
+    /// normalized commit frequency.
+    pub score: f64,
