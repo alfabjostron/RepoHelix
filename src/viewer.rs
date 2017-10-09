befc22d55edac0e1b73899d87fa1432a654e82d2
alfabjostron <23106249+alfabjostron@users.noreply.github.com>
2017-10-09 15:43:00 +0700

    chore: bump rename tracking

---
+++ src/viewer.rs
@@ -0,0 +1,16 @@
+//! Export a compact graph payload for the TypeScript helix/map viewer.
+//!
+//! The viewer needs a small, self-contained JSON document: file nodes with
+//! churn/commit weight, co-change links with strength, and temporal clusters
+//! for the helix strand animation. We cap node count so the payload stays tiny
+//! and the browser render stays smooth without any dependencies.
+
+use crate::json::Json;
+use crate::metrics::Analysis;
+use std::collections::BTreeMap;
+
+/// Build the viewer payload from an analysis, limiting to the most active
+/// `max_nodes` files so the visualization stays legible.
+pub fn build(analysis: &Analysis, max_nodes: usize) -> Json {
+    // Rank files by churn (file_stats is already churn-sorted).
+    let selected: Vec<&str> = analysis
