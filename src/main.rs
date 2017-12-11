c4f25ecb4860d78236d20864329309d425507bcf
alfabjostron <23106249+alfabjostron@users.noreply.github.com>
2017-12-11 11:15:00 +0700

    feat: fixture generation

--- src/main.rs
+++ src/main.rs
@@ -6,3 +6,12 @@ use std::process::ExitCode;
 
 use repohelix::cli::{self, Command, Format, Source};
 use repohelix::metrics::analyze;
+use repohelix::{load_from_log, load_from_repo, report, viewer, Error};
+
+fn main() -> ExitCode {
+    let args: Vec<String> = std::env::args().skip(1).collect();
+    match run(&args) {
+        Ok(()) => ExitCode::SUCCESS,
+        Err(msg) => {
+            let _ = writeln!(std::io::stderr(), "repohelix: {msg}");
+            ExitCode::FAILURE
