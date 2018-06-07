570be57ec5442b751642d9962b7bcf433cd8d2f5
alfabjostron <23106249+alfabjostron@users.noreply.github.com>
2018-06-07 13:36:00 +0700

    chore: commit filtering

--- scripts/gen_fixture.ps1
+++ scripts/gen_fixture.ps1
@@ -17,3 +17,13 @@ $authors = @{
     ada   = @('Ada Reyes',   'ada@nebula.example')
     bo    = @('Bo Tanaka',   'bo@nebula.example')
     cy    = @('Cy Okafor',   'cy@nebula.example')
+    dee   = @('Dee Marsh',   'dee@nebula.example')
+}
+
+# Each commit: hash, author key, unix time, subject, and file numstat rows
+# as "added removed path". Times are chosen to form 4 development sessions
+# (clusters) separated by multi-day gaps.
+$commits = @(
+    # ---- Session 1: bootstrap (day 0) ----
+    @{ h='a1000001'; a='ada'; t=1704067200; s='Bootstrap project layout';
+       f=@('40 0 src/main.rs','25 0 src/lib.rs','12 0 Cargo.toml','30 0 README.md') },
