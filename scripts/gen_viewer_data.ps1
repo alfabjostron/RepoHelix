b913bf30d4311e30700967526f437f1c8f583e09
alfabjostron <23106249+alfabjostron@users.noreply.github.com>
2018-07-06 17:40:00 +0700

    refactor: rename tracking

--- scripts/gen_viewer_data.ps1
+++ scripts/gen_viewer_data.ps1
@@ -1,2 +1,7 @@
 # Generates viewer/data.js from the committed fixture log by invoking the
 # built repohelix binary and wrapping its viewer JSON as a browser global.
+# Run from the repo root:  pwsh -File scripts/gen_viewer_data.ps1
+
+$ErrorActionPreference = 'Stop'
+$root = Join-Path $PSScriptRoot '..'
+Push-Location $root
