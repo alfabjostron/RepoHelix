e809a93e8257c95bfb37b426d38369bf2b6f046c
alfabjostron <23106249+alfabjostron@users.noreply.github.com>
2018-10-25 15:07:00 +0700

    perf: improve fixture generation

--- viewer/test/viewer.test.ts
+++ viewer/test/viewer.test.ts
@@ -24,3 +24,18 @@ function test(name: string, fn: () => void): void {
   passed += 1;
   console.log(`ok - ${name}`);
 }
+
+function sample(): ViewerData {
+  return {
+    schema: "repohelix/viewer/v1",
+    commits: 5,
+    span_days: 12,
+    nodes: [
+      { path: "src/a.rs", churn: 100, commits: 5, authors: 3, concentration: 0.1, first_seen: 100, last_seen: 500 },
+      { path: "src/b.rs", churn: 40, commits: 2, authors: 1, concentration: 1.0, first_seen: 200, last_seen: 300 },
+      { path: "src/c.rs", churn: 0, commits: 1, authors: 1, concentration: 1.0, first_seen: 50, last_seen: 50 },
+    ],
+    links: [{ source: 0, target: 1, strength: 0.5, together: 3 }],
+    clusters: [{ index: 0, start: 50, end: 500, commits: 5, churn: 140 }],
+  };
+}
